0cad891824e25c1909a8df8218575d7ed72880f5
Salvador Jimenez Rosas <157067486+KingChava760177@users.noreply.github.com>
2024-11-04 23:10:04 -0600

    Update DSString.h

--- DSString.h
+++ DSString.h
@@ -8,18 +8,21 @@ private:
     char* data;
     int len;
 
+    void copyData(const char* str);  // Helper function to copy C-string data
+
 public:
-    DSString();                           // Default constructor
-    DSString(const char* str);             // Constructor from C-string
-    DSString(const DSString& other);       // Copy constructor
-    ~DSString();                          // Destructor
+    DSString();                          // Default constructor
+    DSString(const char* str);            // Constructor from C-string
+    DSString(const DSString& other);      // Copy constructor
+    ~DSString();                         // Destructor
 
     DSString& operator=(const DSString& other); // Copy assignment operator
     bool operator==(const DSString& other) const;
     bool operator<(const DSString& other) const;
     char& operator[](int index);
 
-    const char* c_str() const;             // Method to get C-string
+    int length() const;                   // Returns the length of the string
+    const char* c_str() const;            // Method to get C-string
 
     friend std::ostream& operator<<(std::ostream& os, const DSString& str);
 };
