6eb144c2e9f6f94fe8708161716aeba7c56d040f
Salvador Jimenez Rosas <157067486+KingChava760177@users.noreply.github.com>
2024-11-04 23:09:51 -0600

    Update DSString.cpp

--- DSString.cpp
+++ DSString.cpp
@@ -1,25 +1,16 @@
 #include "DSString.h"
-#include <cstring>
 
 // Default constructor
 DSString::DSString() : data(nullptr), len(0) {}
 
 // Constructor from C-string
 DSString::DSString(const char* str) {
-    if (str) {
-        len = strlen(str);
-        data = new char[len + 1];
-        strcpy(data, str);
-    } else {
-        data = nullptr;
-        len = 0;
-    }
+    copyData(str);
 }
 
 // Copy constructor
-DSString::DSString(const DSString& other) : len(other.len) {
-    data = new char[len + 1];
-    strcpy(data, other.data);
+DSString::DSString(const DSString& other) {
+    copyData(other.data);
 }
 
 // Destructor
@@ -27,30 +18,68 @@ DSString::~DSString() {
     delete[] data;
 }
 
+// Helper function to copy data from a C-string
+void DSString::copyData(const char* str) {
+    if (str) {
+        // Calculate the length manually
+        len = 0;
+        while (str[len] != '\0') {
+            len++;
+        }
+        data = new char[len + 1];
+        for (int i = 0; i < len; i++) {
+            data[i] = str[i];
+        }
+        data[len] = '\0';
+    } else {
+        data = nullptr;
+        len = 0;
+    }
+}
+
 // Copy assignment operator
 DSString& DSString::operator=(const DSString& other) {
     if (this != &other) {
         delete[] data;
-        len = other.len;
-        data = new char[len + 1];
-        strcpy(data, other.data);
+        copyData(other.data);
     }
     return *this;
 }
 
 // Equality operator
 bool DSString::operator==(const DSString& other) const {
-    return strcmp(data, other.data) == 0;
+    if (len != other.len) {
+        return false;
+    }
+    for (int i = 0; i < len; i++) {
+        if (data[i] != other.data[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 // Less than operator (for use in map comparisons)
 bool DSString::operator<(const DSString& other) const {
-    return strcmp(data, other.data) < 0;
+    int minLen = len < other.len ? len : other.len;
+    for (int i = 0; i < minLen; i++) {
+        if (data[i] < other.data[i]) {
+            return true;
+        } else if (data[i] > other.data[i]) {
+            return false;
+        }
+    }
+    return len < other.len;
 }
 
 // Access operator
 char& DSString::operator[](int index) {
     return data[index];
+}
+
+// Length of the string
+int DSString::length() const {
+    return len;
 }
 
 // c_str() method for C-string compatibility
